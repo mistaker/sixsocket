f1426883b3b42f9c66e759a49d2d089c1b0ce78f
jiangdong <499576122@qq.com>
2019-11-19 23:49:28 +0800

    epoll logic

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module sixsocket
 
 go 1.13
 
-require golang.org/x/sys v0.0.0-20191119060738-e882bf8e40c2 // indirect
+require golang.org/x/sys v0.0.0-20191119060738-e882bf8e40c2
---
+++ poller/epoll.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"golang.org/x/sys/unix"
+)
+
+type (
+	Poll struct {
+		fd      int
+		eventFd int
+	}
+)
+
+func NewPoll() (*Poll, error) {
+
+	fd, err := unix.EpollCreate1(0)
+	if err != nil {
+		return nil, err
+	}
+
+	r0, _, err := unix.Syscall(unix.SYS_EVENTFD2, 0, 0, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	eventFd := int(r0)
+
+	err = unix.EpollCtl(fd, unix.EPOLL_CTL_ADD, eventFd, &unix.EpollEvent{
+		Events: unix.EPOLLIN,
+		Fd:     int32(eventFd),
+		Pad:    0,
+	})
+
+	if err != nil {
+		_ = unix.Close(fd)
+		_ = unix.Close(eventFd)
+		return nil, err
+	}
+
+	return &Poll{
+		fd:      fd,
+		eventFd: eventFd,
+	}, nil
+}
